92d1abda57c9ed179e89b7486c6ee72d8d40294e
Matt Antonelli <contact@raelys.com>
2018-09-08 18:51:12 +0000

    Update pagination logic for XIVAPI changes
    XIVAPI now returns a PageNext of null if you are on the last page
    (or there are no results.)

--- lib/xivapi/page.rb
+++ lib/xivapi/page.rb
@@ -5,7 +5,6 @@ module XIVAPI
     def initialize(response)
       @results = response['Results']
       @next_page = response['Pagination']['PageNext']
-      @next_page = nil if @next_page == 1
     end
   end
 end
