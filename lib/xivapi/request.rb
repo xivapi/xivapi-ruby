f1356cadd014f3ee67197f96639f04942d309d0e
Matt Antonelli <contact@raelys.com>
2018-09-17 21:24:44 +0000

    Reorganize requests
    Reorganizes requests to follow the ordering of the XIVAPI docs. Also
    adds the `columns` parameter that was missing from character search.

--- lib/xivapi/request.rb
+++ lib/xivapi/request.rb
@@ -4,6 +4,7 @@ module XIVAPI::Request
   LODESTONE_LIMIT = 50.freeze
   ALL_CHARACTER_DATA = 'AC,FR,FC,FCM,PVP'.freeze
 
+  # Search
   def search(indexes: [], string: '', string_column: 'Name_en', string_algo: 'wildcard_plus',
              sort_field: nil, sort_order: nil, limit: 100, filters: [], columns: [])
     params = { indexes: [*indexes].join(','), string: string, string_column: string_column, string_algo: string_algo,
@@ -11,13 +12,30 @@ module XIVAPI::Request
     XIVAPI::Paginator.new(self, params, 'search', limit)
   end
 
+  # Content
+  def content(name: nil, ids: [], limit: 100, columns: [])
+    if name.nil?
+      request(self, 'content')
+    elsif [*ids].size == 1
+      request("#{name.capitalize}/#{[*ids].first}")
+    else
+      params = { ids: [*ids].join(','), columns: [*columns].join(',') }
+      XIVAPI::Paginator.new(self, params, name.capitalize, limit)
+    end
+  end
+
+  def servers
+    request(self, 'servers')
+  end
+
+  # Character
   def character(id: nil, all_data: false, poll: false, columns: [])
     params = { data: all_data ? ALL_CHARACTER_DATA : nil, columns: [*columns].join(',') }
     request_cached(self, "character/#{id}", :character, params, poll)
   end
 
-  def character_search(name: nil, server: nil)
-    params = { name: name, server: server&.capitalize }
+  def character_search(name: nil, server: nil, columns: [])
+    params = { name: name, server: server&.capitalize, columns: [*columns].join(',') }
     XIVAPI::Paginator.new(self, params, 'character/search', LODESTONE_LIMIT)
   end
 
@@ -32,20 +50,5 @@ module XIVAPI::Request
 
   def character_verified?(id: nil)
     request(self, "character/#{id}/verification").verification_token_pass
-  end
-
-  def content(name: nil, ids: [], limit: 100, columns: [])
-    if name.nil?
-      request(self, 'content')
-    elsif [*ids].size == 1
-      request("#{name.capitalize}/#{[*ids].first}")
-    else
-      params = { ids: [*ids].join(','), columns: [*columns].join(',') }
-      XIVAPI::Paginator.new(self, params, name.capitalize, limit)
-    end
-  end
-
-  def servers
-    request(self, 'servers')
   end
 end
